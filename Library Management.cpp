170487762b13959b087a3bc773efd120814a20fe
satwikpadhy <33980070+satwikpadhy@users.noreply.github.com>
2021-01-24 20:43:15 +0530

    Porting From Windows to Linux
    Porting not complete yet. Work Still required.
    And Yes I regret using system() function in the program

--- Library Management.cpp
+++ Library Management.cpp
@@ -11,46 +11,46 @@ class book
  		char bname[40];
  		int bno;
  		char aname[40];
- 	
+
 	public:
- 	
+
 	void create_book()
- 	 {  
- 	    system("cls");
+ 	 {
+ 	    system("clear");
  	 	cout<<"Enter book number :";
  	 	cin>>bno;
  	 	cout<<"Enter book name :";
  	 	cin.get();
-		gets(bname);
+		fgets(bname, sizeof(bname), stdin);
  	 	cout<<"Enter Author name :";
- 	 	gets(aname);
+ 	 	fgets(aname, sizeof(aname), stdin);
  	 }
- 	
+
  	void show_book()
  	 {
  	 	cout<<"Book Number :"<<bno<<endl;
  	 	cout<<"Book Name :"; puts(bname);
  	 	cout<<"Author Name :"; puts(aname);
  	 }
- 	
+
  	void modify_book()
- 	 {  
+ 	 {
  	    cout<<"Enter new Book Number:";
  	    cin>>bno;
  	 	cout<<"Enter new book name :";
  	 	cin.get();
- 	 	gets(bname);
+ 	 	fgets(bname, sizeof(bname), stdin);
  	 	cout<<"Enter new author name :";
- 	    gets(aname);
+ 	    fgets(aname, sizeof(aname), stdin);
  	 }
- 	 
+
  	void bookrepo()
  	 {
  	 	cout<<"Book Number :"<<bno<<endl;
  	 	cout<<"Book Name :" ; puts(bname);
  	 	cout<<"Author Name :"; puts(aname);
  	 }
- 	 
+
  	int booknumber()
  	 {
  	 	return bno;
@@ -65,31 +65,31 @@ class student
  		char sname[25];
  		int bno;
  		int token;
- 		
+
  	public :
  		void create_s()
- 		 {  
+ 		 {
  		    system ("cls");
  		 	cout<<"Enter The admission number of the student :";
  		 	cin>>admno;
  		 	cout<<"Enter Student Name :";
  		 	cin.get();
- 		 	gets(sname);
+ 		 	fgets(sname, sizeof(sname), stdin);
  		 	token = 0;
  		 	system ("cls");
  		 	cout<<"New Record Created!";
  		 }
- 		
+
  		void show_s()
- 		 {  
+ 		 {
  		 	cout<<endl;
 			cout<<"Admission Number :"<<admno<<endl;
  		 	cout<<"Student Name :"; puts(sname);
  		 	cout<<"Number of Books Issued :"<<token<<endl;
  		 	cout<<"Book Number :"<<bno<<endl;
  		 }
- 		
- 		int admissionno()  
+
+ 		int admissionno()
           {
 		    return admno;
 		  }
@@ -101,16 +101,16 @@ class student
 		 {
 		 	return token;
 		 }
-		 
+
 		void modifystu()
 		 {
-		 	system ("cls");
+		 	system ("clear");
  		 	cout<<"Enter The admission number of the student :";
  		 	cin>>admno;
  		 	cout<<"Enter Student Name :";
  		 	cin.get();
- 		 	gets(sname);
- 		 	system ("cls");
+ 		 	fgets(sname, sizeof(sname), stdin);
+ 		 	system ("clear");
  		 	cout<<"Record Modified!";
  	     }
 		 void addtoken()
@@ -127,35 +127,35 @@ class student
  		  	bno= x;
  		  }
  };
- 
- 
- 
 
- 	
- 
-//################################################################################################################################## 
+
+
+
+
+
+//##################################################################################################################################
 /*Global Declarations#############################################################################################################
-##################################################################################################################################*/ 
- 
+##################################################################################################################################*/
+
  fstream f,fd;
  student s;
  book b;
- 
+
 
 //####################################################################################################################################
 //Functions Used in the program. #####################################################################################################
-//#################################################################################################################################### 		
+//####################################################################################################################################
 void startup()
  {
- 	system("cls");
+ 	system("clear");
  	cout<<"Kendriya Vidylaya No. 2 CRPF Campus, Bhubaneswar \n\nLibrary Management \n\n";
- 	system("pause");
- 	system("cls");
+ 	cout<<"Press Any Key to continue :"; cin.get();;
+ 	system("clear");
  }
-	
+
 void book_issue()
  {
- 	system("cls");
+ 	system("clear");
  	int adm,bn,t=0,t1=0;
  	cout<<"Issue a Book"<<endl;
  	cout<<"Enter the Student's Admission Number :";
@@ -171,10 +171,10 @@ void book_issue()
  	 	  	 {
  	 	  	    cout<<"Enter the Book Number of the book to be issued :";
 				cin>>bn;
-				while(fd.read((char*)&b,sizeof(book)))	
+				while(fd.read((char*)&b,sizeof(book)))
 				 {
 				 	if(b.booknumber()==bn)
-				 	 { 
+				 	 {
 				 	    cout<<"Book Details "<<endl;
 				 	 	b.show_book();
 				 	 	cout<<"\n\nStudent Details \n";
@@ -182,14 +182,14 @@ void book_issue()
 				 	 	s.addtoken();
 				 	 	s.get_bno(bn);
 				 	 	t1=1;
-				 	 	
-				 	 	int pos=-1*sizeof(s);
+
+				 	 	long int pos=-1*sizeof(s);
 				 	 	f.seekp(pos,ios::cur);
 				 	 	f.write((char*)&s,sizeof(student));
 				 	 	cout<<"Book Issued Successfully!"<<endl;
 				 	 	cout<<"Please Note : Write the Date in the Back of the Book. The book is to be returned within 15 days. Fine Rs5 per day of delay."<<endl;
 				 	 }
-				 	
+
 			     }
 			    if (t1==0)
 			     cout<<"Book Not Found!";
@@ -197,50 +197,50 @@ void book_issue()
  	 	    else
  	 	     cout<<"You may not have returned previous book.";
  	 	  }
- 	 	
+
  	 }
  	if(t==0)
  	 	 cout<<"Student Not found.";
-    system("pause");
+    ;
     f.close();
     fd.close();
  }
 
 void book_deposit()
  {  int adm,t=0;
- 	system("cls");
+ 	system("clear");
  	cout<<"Deposite Book."<<endl;
  	cout<<"Enter the Admission Number of the Student :";
     cin>>adm;
     f.open("students.dat",ios::in|ios::out);
-    
+
     while(f.read((char*)&s,sizeof(student)))
      {
      	if(adm==s.admissionno())
-     	 {  
+     	 {
      	    if(s.no_of_books()==1)
      	 	{
 			cout<<"Details of the Student :"<<endl;
      	 	s.show_s();
      	    s.reset_token();
-     	    int pos=-1*sizeof(s);
+     	    long int pos=-1*sizeof(s);
 			f.seekp(pos,ios::cur);
 			f.write((char*)&s,sizeof(student));
 			cout<<"Book Return Successfull!"<<endl;
 		    }
 		    else
 		     cout<<"No books Issued.";
-		     
-		    t=1; 
+
+		    t=1;
          }
      }
     if(t==0)
      cout<<"Student Not Found!";
     f.close();
-    system("pause");
+    cout<<"Press Any Key to continue :"; cin.get();;
  }
- 
- 
+
+
 
 
 //############################################################################################################
@@ -254,72 +254,71 @@ void write_book()
  {
  	char ch;
     f.open("books.dat",ios::out|ios::app);
-	do 
+	do
 	 {
 	   b.create_book();
-	   f.write((char*)&b,sizeof(book)); 
+	   f.write((char*)&b,sizeof(book));
 	   cout<<"Do you want to add more books?(y/n) :";
 	   cin>>ch;
      }while(ch=='y'||ch=='Y');
     f.close();
  }
- 
+
 void write_stu()
  {
  	char ch;
     f.open("students.dat",ios::out|ios::app);
-	do 
+	do
 	 {
 	   s.create_s();
-	   f.write((char*)&s,sizeof(student)); 
+	   f.write((char*)&s,sizeof(student));
 	   cout<<"Do you want to add more Students?(y/n) :";
 	   cin>>ch;
      }while(ch=='y'||ch=='Y');
     f.close();
  }
- 
- 
+
+
 
 
 
 
 //############################################################################################################
 //Functions to display all Details############################################################################
-//############################################################################################################ 
- 
- 
- 
- 
- 
+//############################################################################################################
+
+
+
+
+
 void disp_all_b()
  {
    f.open("books.dat",ios::in);
-   system("cls");
+   system("clear");
    while(!f.eof())
-    { 
+    {
       cout<<endl;
 	  f.read((char*)&b,sizeof(book));
 	  if(f.eof()) break;
 	  b.show_book();
     }
     f.close();
-    system("pause");
+    cout<<"Press Any Key to continue :"; cin.get();
  }
- 
+
 
 void disp_all_s()
  {
    f.open("students.dat",ios::in);
-   system("cls");
+   system("clear");
    while(!f.eof())
-    { 
+    {
       cout<<endl;
 	  f.read((char*)&s,sizeof(student));
 	  if(f.eof()) break;
 	  s.show_s();
     }
     f.close();
-    system("pause");
  }
 
 
@@ -327,15 +326,15 @@ void disp_all_s()
 
 //############################################################################################################
 //## Functions to display specific Details ###################################################################
-//############################################################################################################ 
+//############################################################################################################
 
 
 
 
 void disp_spec_b()
- {  
+ {
     int scr,t=0;
- 	system("cls");
+ 	system("clear");
  	f.open("books.dat",ios::in);
  	cout<<"Enter Book Number of the book you want to search for:";
  	cin>>scr;
@@ -345,27 +344,27 @@ void disp_spec_b()
  	  if(f.eof())
  	 	    break;
  	  if(scr==b.booknumber())
- 	 	 { 
+ 	 	 {
 		   b.show_book();
 		   t=1;
          }
-	  
+
 	}
-		
-	   
+
+
 	if(t==0)
 	    cout<<"Sorry Book Not Found. Try Again.\n";
-	  
+
     f.close();
-    system("pause");
+    cout<<"Press Any Key to continue :"; cin.get();;
 }
 
 
 
 void disp_spec_s()
- {  
+ {
     int scr,t=0;
- 	system("cls");
+ 	system("clear");
  	f.open("students.dat",ios::in);
  	cout<<"Enter admission Number of the Student you want to search for:";
  	cin>>scr;
@@ -375,19 +374,19 @@ void disp_spec_s()
  	  if(f.eof())
  	 	    break;
  	  if(scr==s.admissionno())
- 	 	 { 
+ 	 	 {
 		   s.show_s();
 		   t=1;
          }
-	  
+
 	}
-		
-	   
+
+
 	if(t==0)
 	    cout<<"Sorry Student Not Found. Try Again.\n";
-	  
+
     f.close();
-    system("pause");
+    cout<<"Press Any Key to continue :"; cin.get();;
 }
 
 
@@ -400,64 +399,64 @@ void disp_spec_s()
 
 
 void modify_b()
- { 
+ {
    int scr,t=0;
- 	system("cls");
+ 	system("clear");
  	f.open("books.dat",ios::in|ios::out);
  	cout<<"Enter Book Number of the book you want to modify:";
  	cin>>scr;
  	while(f.read((char*)&b,sizeof(book)))
  	{
- 	  
+
  	  if(scr==b.booknumber())
- 	 	 { 
+ 	 	 {
 		   b.modify_book();
 		   t=1;
-		   int pos = -1*sizeof(b);
+		   long int pos = -1*sizeof(b);
 	       f.seekp(pos,ios::cur);
-	       f.write((char*)&b,sizeof(book));  
+	       f.write((char*)&b,sizeof(book));
          }
-	  
+
 	}
-		
-	   
+
+
 	if(t==0)
 	    cout<<"Sorry Book Not Found. Try Again.\n";
-	
+
 	f.close();
-    system("pause");
-    
+    cout<<"Press Any Key to continue :"; cin.get();;
+
 }
 
 
 void modify_s()
- { 
+ {
    int scr,t=0;
- 	system("cls");
+ 	system("clear");
  	f.open("students.dat",ios::in|ios::out);
  	cout<<"Enter Admission Number of the Student you want to modify:";
  	cin>>scr;
  	while(f.read((char*)&s,sizeof(student)))
  	{
- 	  
+
  	  if(scr==s.admissionno())
- 	 	 { 
+ 	 	 {
 		   s.modifystu();
 		   t=1;
-		   int pos = -1*sizeof(s);
+		   long int pos = -1*sizeof(s);
 	       f.seekp(pos,ios::cur);
-	       f.write((char*)&s,sizeof(student));  
+	       f.write((char*)&s,sizeof(student));
          }
-	  
+
 	}
-		
-	   
+
+
 	if(t==0)
 	    cout<<"Sorry Student Not Found. Try Again.\n";
-	
+
 	f.close();
-    system("pause");
-    
+    cout<<"Press Any Key to continue :"; cin.get();;
+
 }
 
 
@@ -469,7 +468,7 @@ void modify_s()
 
 
 void del_b()
- {  system("cls");
+ {  system("clear");
     int bn;
  	cout<<"Enter the Book Number of the book you want to remove :";
  	cin>>bn;
@@ -488,11 +487,11 @@ void del_b()
  	remove("books.dat");
  	rename("temp.dat","books.dat");
  	cout<<"\nBook Deleted";
- 	system("pause");
+ 	cout<<"Press Any Key to continue :"; cin.get();;
  }
- 
+
 void del_s()
- {  system("cls");
+ {  system("clear");
     int bn;
  	cout<<"Enter the Admission Number of the Student you want to remove :";
  	cin>>bn;
@@ -511,26 +510,26 @@ void del_s()
  	remove("students.dat");
  	rename("temp.dat","students.dat");
  	cout<<"\nStudent Record Deleted";
- 	system("pause");
+ 	cout<<"Press Any Key to continue :"; cin.get();;
  }
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
+
+
+
+
+
+
+
+
+
 //####################################################################################################################################
 //Function for Admin Menu############################################################################################################
-//###################################################################################################################################	
- 	
- 
+//###################################################################################################################################
+
+
 void admin_menu()
 {   int ch;
 	do {
-		system("cls");
+		system("clear");
 	    cout<<"Administration Menu\n";
 	    cout<<"\n1.Create Student Record.";
 	    cout<<"\n2.Display all Student Record";
@@ -545,10 +544,10 @@ void admin_menu()
 	    cout<<"\n11.Return to main menu";
 	    cout<<"\nEnter Option (1-11) :";
 	    cin>>ch;
-	
+
 	    switch (ch)
         {
-          case 1: write_stu(); break;	
+          case 1: write_stu(); break;
 		  case 2: disp_all_s(); break;
 		  case 3: disp_spec_s(); break;
 		  case 4: modify_s(); break;
@@ -573,19 +572,19 @@ void admin_menu()
 
 
 int main()
- {  
+ {
     int ch;
  	startup();
  	do
- 	{   system("cls");
+ 	{   system("clear");
  		cout<<" \t\t Main Menu \n";
  		cout<<"1. Book Issue \n";
  		cout<<"2. Book Deposit \n";
  		cout<<"3. Administration Menu \n";
  		cout<<"4. Exit \n Select option (1-4)";
- 		
+
  		cin>>ch;
- 		system("cls");
+ 		system("clear");
  		switch(ch)
  		 {
  		 	case 1 : book_issue();
@@ -597,6 +596,6 @@ int main()
  		 	case 4 : break;
  		 	case 5 : cout<<"Wrong Option. Please try again.";
  		 }
- 		 	
+
  	}while(ch!=4);
  }
